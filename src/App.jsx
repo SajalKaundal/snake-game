fe825b03a50ba8f4677120fafeec3c28ea388480
Sajal Kaundal <sajal4242kaundal@gmail.com>
2026-04-17 15:26:00 +0530

    added key controls to the square direction can be change using arrow keys and awsd keys

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,68 @@ import "./App.css";
 
 function App() {
   const [topPosition, setTopPosition] = useState(0);
-  const [leftPostion, setLeftPosition] = useState(0);
-
+  const [leftPosition, setLeftPosition] = useState(0);
+  const [direction, setDirection] = useState("Up");
   useEffect(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
     const handleKeyDown = (e) => {
-      console.log(e.key);
-      if (e.key === "ArrowUp") {
-        setTopPosition((prev) => prev - 10);
-      } else if (e.key === "ArrowDown") {
-        setTopPosition((prev) => prev + 10);
-      } else if (e.key === "ArrowRight") {
-        setLeftPosition((prev) => prev + 10);
-      } else if (e.key === "ArrowLeft") {
-        setLeftPosition((prev) => prev - 10);
+      const key = e.key.toUpperCase()
+      if (key === "ARROWUP"|| key === "W") {
+        // setTopPosition((prev) => prev - 10);
+        setDirection("Up");
+      } else if (key === "ARROWDOWN"|| key === "S") {
+        // setTopPosition((prev) => prev + 10);
+        setDirection("Down");
+      } else if (key === "ARROWRIGHT"|| key === "D") {
+        // setLeftPosition((prev) => prev + 10);
+        setDirection("Right");
+      } else if (key === "ARROWLEFT"|| key === "A") {
+        // setLeftPosition((prev) => prev - 10);
+        setDirection("Left");
       }
     };
     window.addEventListener("keydown", handleKeyDown);
 
-    return window.removeEventListener("keydown",handleKeyDown)
-  }, []);
+    const interval = setInterval(() => {
+      if (direction === "Up") {
+        setTopPosition((prev) => {
+          if (prev < 0) {
+            return height;
+          }
+          return prev - 1;
+        });
+      } else if (direction === "Down") {
+        setTopPosition((prev) => {
+          if (prev > height) {
+            return 0;
+          }
+          return prev + 1;
+        });
+      } else if (direction === "Left") {
+        setLeftPosition((prev) => {
+          if (prev < 0) {
+            return width;
+          }
+          return prev - 1;
+        });
+      } else if (direction === "Right") {
+        setLeftPosition((prev) => {
+          if (prev > width) {
+            return 0;
+          }
+          return prev + 1;
+        });
+      }
+    }, 2);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      clearInterval(interval);
+    };
+  }, [direction]);
 
-  console.log(`topPosition ${topPosition}`);
-  console.log(`leftPostion ${leftPostion}`);
+  // console.log(`topPosition ${topPosition}`);
+  // console.log(`leftPostion ${leftPosition}`);
   return (
     <>
       <div style={{ width: "100vw", height: "100vh" }}>
@@ -35,7 +75,7 @@ function App() {
             backgroundColor: "blue",
             position: "fixed",
             top: topPosition,
-            left: leftPostion,
+            left: leftPosition,
           }}
         ></div>
       </div>
